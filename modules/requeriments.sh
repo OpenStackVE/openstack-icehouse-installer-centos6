b8dd8e73cef8789c214f3c4bca61cd4d74b0de1c
Reynaldo R. Martinez P <tigerlinux@gmail.com>
2014-04-16 17:25:51 -0430

    Commit para versión 1.0.0.RC1 "Snow Lynx"
    - Se prueba con la versión RC1 en RDO.
    - Cambios generales para adaptar el instalador original
      para Havana en la nueva versión (Icehouse).
    - Se utilizar MariaDB 5.5.x en lugar de MySQL - También
      de los repositorios RDO.
    - Se cambia nuestro message broker por defecto a RabbitMQ.
    - Todos los módulos probados y operativos.

---
+++ modules/requeriments.sh
@@ -0,0 +1,238 @@
+#!/bin/bash
+#
+# Instalador desatendido para Openstack Icehouse sobre CENTOS
+# Reynaldo R. Martinez P.
+# E-Mail: TigerLinux@Gmail.com
+# Abril del 2014
+#
+# Script de instalacion y preparacion de pre-requisitos
+#
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+if [ -f ./configs/main-config.rc ]
+then
+	source ./configs/main-config.rc
+	mkdir -p /etc/openstack-control-script-config
+else
+	echo "No puedo acceder a mi archivo de configuración"
+	echo "Revise que esté ejecutando el instalador/módulos en el directorio correcto"
+	echo "Abortando !!!!."
+	echo ""
+	exit 0
+fi
+
+#
+# Verificaciones y taras de limpieza iniciales para evitar "opppss"
+#
+
+rm -rf /tmp/keystone-signing-*
+rm -rf /tmp/cd_gen_*
+
+epelinstalled=`rpm -qa|grep epel-release.\*noarch|wc -l`
+amiroot=` whoami|grep root|wc -l`
+amiarhel6=`cat /etc/redhat-release |grep 6.|wc -l`
+internalbridgeinterface=`ifconfig $integration_bridge|grep -c $integration_bridge`
+internalbridgepresent=`ovs-vsctl show|grep -i -c bridge.\*$integration_bridge`
+oskernelinstalled=`uname -r|grep -c x86_64`
+
+	
+echo "Instalando paquetes iniciales"
+echo ""
+
+# Se instalan las dependencias principales vía yum
+#
+yum -y clean all
+yum -y install yum-plugin-priorities yum-presto yum-plugin-changelog openstack-packstack
+yum -y groupinstall Virtualization-Platform Virtualization-tools
+yum -y install sudo gcc cpp make automake kernel-headers
+yum -y install python-keystoneclient python-sqlalchemy0.7 python-migrate python-psycopg2 \
+	MySQL-python python-tools sysfsutils sg3_utils genisoimage libguestfs glusterfs \
+	glusterfs-fuse nfs-utils sudo sheepdog libguestfs-tools-c
+
+yum -y install boost-program-options perl-DBD-MySQL wxBase wxGTK  wxGTK-gl libtool-ltdl \
+	unixODBC python-six python-iso8601 python-babel python-argparse python-oslo-config \
+	python-ordereddict python-webob python-memcached python-oauthlib python-routes1.12 \
+	python-backports python-backports-ssl_match_hostname python-urllib3 python-passlib \
+	python-dogpile-core python-dogpile-cache python-jsonschema python-paste \
+	python-paste-deploy1.5 python-tempita python-chardet python-requests python-stevedore
+
+yum -y install PyPAM python-decorator python-migrate python-prettytable python-keyring \
+	python-keystoneclient python-greenlet python-eventlet python-oslo-messaging \
+	python-pycadf python-keystone python-httplib2 pyxattr python-swiftclient \
+	python-kombu python-qpid pysendfile python-jsonpointer python-jsonpatch \
+	python-warlock python-glanceclient python-simplejson python-cinderclient \
+	saslwrapper python-saslwrapper python-glance crudini libibverbs librdmacm
+
+yum -y install perl-Config-General python-anyjson python-novaclient python-amqplib \
+	python-markdown python-oslo-rootwrap python-suds libyaml PyYAML python-pygments \
+	python-cheetah pyparsing python-futures python-lockfile python-devel numpy-f2py \
+	scipy python-networkx-core python-taskflow python-cinder python-markupsafe \
+	python-jinja2 python-pyasn1 python-boto python-cmd2 python-cliff
+
+yum -y install python-neutronclient python-websockify pysnmp python-croniter python-beaker \
+	python-mako python-alembic python-ply python-msgpack python-jsonpath-rw \
+	python-ceilometer python-libguestfs python-neutron python-ceilometerclient \
+	python-troveclient python-versiontools python-bson python-pymongo python-simplegeneric \
+	python-logutils python-werkzeug python-flask python-webtest python-ipaddr \
+	python-wsme python-singledispatch python-pecan 
+
+yum -y install python-ceilometerclient python-cinderclient python-glanceclient \
+	python-heatclient python-openstackclient python-swiftclient python-neutronclient \
+	python-novaclient python-configobj python-lesscpy python-netifaces
+	
+yum -y install tuned tuned-utils
+echo "virtual-host" > /etc/tune-profiles/active-profile
+chkconfig ksm on
+chkconfig ksmtuned on
+chkconfig tuned on
+
+service ksm restart
+service ksmtuned restart
+service tuned restart
+
+testlibvirt=`rpm -qi libvirt|grep -ci "is not installed"`
+
+if [ $testlibvirt == "1" ]
+then
+	echo ""
+	echo "Falló la instalación del prerequisito libvirt - abortando el resto de la instalación"
+	echo ""
+	exit 0
+fi
+
+packstackinstalled=`rpm -qa|grep openstack-packstack.\*noarch|grep -v puppet|wc -l`
+searchtestnova=`yum search openstack-nova-common|grep openstack-nova-common.\*noarch|wc -l`
+
+
+if [ $amiarhel6 == "1" ]
+then
+	echo ""
+	echo "Ejecutando en un RHEL6 o Compatible - continuando"
+	echo ""
+else
+	echo ""
+	echo "No se pudo verificar que el sistema operativo es un RHEL6 o Compatible"
+	echo "Abortando"
+	echo ""
+fi
+
+if [ $epelinstalled == "1" ]
+then
+	echo ""
+	echo "Epel Instalado - continuando"
+else
+	echo ""
+	echo "Prerequisito inexistente: Repositorio EPEL no instalado"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+if [ $amiroot == "1" ]
+then
+	echo ""
+	echo "Ejecutando como root - continuando"
+	echo ""
+else
+	echo ""
+	echo "ALERTA !!!. Este script debe ser ejecutado por el usuario root"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+if [ $oskernelinstalled == "1" ]
+then
+	echo ""
+	echo "Kernel OpenStack RDO x86_64 detectado - continuando"
+	echo ""
+	else
+	echo ""
+	echo "ALERTA !!!. Este servidor no tiene el Kernel de RDO-Openstack instalado"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+if [ $packstackinstalled == "1" ]
+then
+	echo ""
+	echo "Packstack instalado correctamente - continuando"
+	echo ""
+else
+	echo ""
+	echo "No se pudo verificar la existencia de Packstack"
+	echo "Posible falla con repositorio RDO"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+if [ $searchtestnova == "1" ]
+then
+	echo ""
+	echo "Repositorios RDO aparentemente en orden - continuando"
+	echo ""
+else
+	echo ""
+	echo "No se pudo verificar el correcto funcionamiento del repo RDO"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+if [ $internalbridgeinterface == "1" ]
+then
+	echo ""
+	echo "Interfaz del bridge de integracion Presente - Continuando"
+	echo ""
+else
+	echo ""
+	echo "No se pudo encontrar la interfaz del bridge de integracion"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+if [ $internalbridgepresent == "1" ]
+then
+	echo ""
+	echo "Bridge de integracion Presente - Continuando"
+	echo ""
+else
+	echo ""
+	echo "No se pudo encontrar el bridge de integracion"
+	echo "Abortando"
+	echo ""
+	exit 0
+fi
+
+echo ""
+echo "Pre-requisitos iniciales validados"
+echo ""
+
+echo "Preparando libvirt y limpiando configuración de IPTABLES"
+echo "No se preocupe si ve un mensaje de FAILED"
+
+if [ -f /etc/openstack-control-script-config/libvirt-installed ]
+then
+	echo "Libvirt y otros prerequisitos ya instalados"
+else
+	service libvirtd stop
+	rm /etc/libvirt/qemu/networks/autostart/default.xml
+	rm /etc/libvirt/qemu/networks/default.xml
+	service iptables stop
+	echo “” > /etc/sysconfig/iptables
+	service libvirtd start
+	chkconfig libvirtd on
+	service iptables save
+	service iptables start
+	iptables -A INPUT -p tcp -m multiport --dports 22 -j ACCEPT
+	service iptables save
+	date > /etc/openstack-control-script-config/libvirt-installed
+fi
+
+
+
+
---
+++ modules/uninstall.sh
@@ -0,0 +1,209 @@
+#!/bin/bash
+#
+# Instalador desatendido para Openstack Icehouse sobre CENTOS
+# Reynaldo R. Martinez P.
+# E-Mail: TigerLinux@Gmail.com
+# Abril del 2014
+#
+# Script de desinstalacion de OS para Centos 6
+#
+
+PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+
+if [ -f ./configs/main-config.rc ]
+then
+	source ./configs/main-config.rc
+else
+	echo "No puedo acceder a mi archivo de configuración"
+	echo "Revise que esté ejecutando el instalador en su directorio"
+	echo "Abortando !!!!."
+	echo ""
+	exit 0
+fi
+
+clear
+
+echo "Bajando y desactivando Servicios de OpenStack"
+
+/usr/local/bin/openstack-control.sh stop
+/usr/local/bin/openstack-control.sh disable
+service mongod stop
+chkconfig mongod off
+killall -9 -u mongodb
+killall -9 mongod
+killall -9 dnsmasq
+
+echo "Eliminando Paquetes de OpenStack"
+
+yum -y erase openstack-glance \
+	openstack-utils \
+	openstack-selinux \
+	openstack-keystone \
+	python-psycopg2 \
+	qpid-cpp-server \
+	qpid-cpp-server-ssl \
+	qpid-cpp-client \
+	scsi-target-utils \
+	sg3_utils \
+	openstack-cinder \
+	openstack-neutron \
+	openstack-neutron-* \
+	openstack-nova-* \
+	openstack-swift-* \
+	openstack-ceilometer-* \
+	openstack-heat-* \
+	mongodb-server \
+	mongodb \
+	haproxy \
+	rabbitmq-server \
+	openstack-dashboard \
+	openstack-packstack \
+	sysfsutils \
+	genisoimage \
+	libguestfs \
+	rabbitmq-server \
+	python-django-openstack-auth \
+	python-keystone*
+
+if [ $cleanundeviceatuninstall == "yes" ]
+then
+	rm -rf /srv/node/$swiftdevice/accounts
+	rm -rf /srv/node/$swiftdevice/containers
+	rm -rf /srv/node/$swiftdevice/objects
+	rm -rf /srv/node/$swiftdevice/tmp
+	chown -R root:root /srv/node/
+	restorecon -R /srv
+fi
+
+echo "Eliminando Usuarios de Servicios de OpenStack"
+
+userdel -f -r keystone
+userdel -f -r glance
+userdel -f -r cinder
+userdel -f -r neutron
+userdel -f -r nova
+userdel -f -r mongodb
+userdel -f -r ceilometer
+userdel -f -r swift
+userdel -f -r rabbitmq
+userdel -f -r heat
+
+echo "Eliminando Archivos remanentes"
+
+rm -fr /etc/glance \
+	/etc/keystone \
+	/var/log/glance \
+	/var/log/keystone \
+	/var/lib/glance \
+	/var/lib/keystone \
+	/etc/cinder \
+	/var/lib/cinder \
+	/var/log/cinder \
+	/etc/sudoers.d/cinder \
+	/etc/tgt \
+	/etc/neutron \
+	/var/lib/neutron \
+	/var/log/neutron \
+	/etc/sudoers.d/neutron \
+	/etc/nova \
+	/etc/heat \
+	/var/log/nova \
+	/var/lib/nova \
+	/etc/sudoers.d/nova \
+	/etc/openstack-dashboard \
+	/var/log/horizon \
+	/etc/sysconfig/mongod \
+	/var/lib/mongodb \
+	/etc/ceilometer \
+	/var/log/ceilometer \
+	/var/lib/ceilometer \
+	/etc/ceilometer-collector.conf \
+	/etc/swift/ \
+	/var/lib/swift \
+	/tmp/keystone-signing-swift \
+	/etc/openstack-control-script-config \
+	/var/lib/keystone-signing-swift \
+	/var/lib/rabbitmq \
+	$dnsmasq_config_file \
+	/etc/dnsmasq-neutron.d \
+	/var/tmp/packstack \
+	/var/lib/keystone-signing-swift
+
+
+service crond restart
+
+rm -f /root/keystonerc_admin
+rm -f /root/ks_admin_token
+rm -f /usr/local/bin/openstack-control.sh
+rm -f /usr/local/bin/openstack-log-cleaner.sh
+rm -f /etc/httpd/conf.d/openstack-dashboard.conf*
+rm -f /etc/httpd/conf.d/rootredirect.conf*
+
+
+if [ $snmpinstall == "yes" ]
+then
+	if [ -f /etc/snmp/snmpd.conf.pre-openstack ]
+	then
+		rm -f /etc/snmp/snmpd.conf
+		mv /etc/snmp/snmpd.conf.pre-openstack /etc/snmp/snmpd.conf
+		service snmpd restart
+	else
+		service snmpd stop
+		yum -y erase net-snmp
+		rm -rf /etc/snmp
+	fi
+	rm -f /usr/local/bin/vm-number-by-states.sh \
+	/usr/local/bin/vm-total-cpu-and-ram-usage.sh \
+	/usr/local/bin/vm-total-disk-bytes-usage.sh \
+	/usr/local/bin/node-cpu.sh \
+	/usr/local/bin/node-memory.sh \
+	/etc/cron.d/openstack-monitor.crontab \
+	/var/tmp/node-cpu.txt \
+	/var/tmp/node-memory.txt \
+	/var/tmp/vm-cpu-ram.txt \
+	/var/tmp/vm-disk.txt \
+	/var/tmp/vm-number-by-states.txt
+fi
+
+echo "Reiniciando Apache sin archivos del Dashboard"
+
+service httpd restart
+service memcached restart
+
+echo "Limpiando IPTABLES"
+
+service iptables stop
+echo "" > /etc/sysconfig/iptables
+
+if [ $dbinstall == "yes" ]
+then
+
+	echo ""
+	echo "Desinstalando software de Base de Datos"
+	echo ""
+	case $dbflavor in
+	"mysql")
+		service mysqld stop
+		sync
+		sleep 5
+		sync
+		yum -y erase mysql-server mysql mariadb-galera-server mariadb-galera-common mariadb-galera galera
+		userdel -r mysql
+		rm -f /root/.my.cnf /etc/my.cnf
+		;;
+	"postgres")
+		service postgresql stop
+		sync
+		sleep 5
+		sync
+		yum -y erase postgresql-server
+		userdel -r postgres
+		rm -f /root/.pgpass
+		;;
+	esac
+fi
+
+echo ""
+echo "Desinstalación completada"
+echo ""
+
